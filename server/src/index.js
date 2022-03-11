57bc0ec657acacdd3982f53a9f45c9526d1ec14d
Krushna Katore <91548932+krushnakatore@users.noreply.github.com>
2022-03-11 09:44:02 +0530

    cors changed

--- server/src/index.js
+++ server/src/index.js
@@ -6,11 +6,7 @@ app.use(express.json());
 
 const cors = require("cors");
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+app.use(cors());
 
 const shopController = require("./controllers/shop.controller");
 
